8558704bc80eeea6fe5605f87cbbd4e3cfa94337
Tom White <tomwhite007@gmail.com>
2022-09-08 11:58:26 +0100

    feat: weather api service

---
+++ src/app/forecast/services/weather-api.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { FiveDayForecastApiResult } from '../models/five-day-forecast-api-result';
+
+@Injectable({
+  providedIn: 'any',
+})
+export class WeatherApiService {
+  constructor(private http: HttpClient) {}
+
+  getFiveDayForecast(city: string) {
+    const url = environment.api.fiveDayForecastUrl
+      .replace('{apiKey}', environment.api.openWeatherMapApiKey)
+      .replace('{countryCode}', environment.api.ukCountryCode)
+      .replace('{cityName}', city);
+
+    return this.http.get<FiveDayForecastApiResult>(url);
+  }
+}
