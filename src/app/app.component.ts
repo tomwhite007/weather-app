8531250a29d860f578355c026d9aedd4cae43fcd
Tom White <tomwhite007@gmail.com>
2022-09-08 18:14:25 +0100

    feat: sprite sheet and title weather icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Weather App';
+
+  constructor(private vcr: ViewContainerRef) {}
+
+  ngOnInit(): void {
+    setTimeout(async () => {
+      const { LazySvgSpriteSheetComponent } = await import(
+        './core/lazy-svg-sprite-sheet/lazy-svg-sprite-sheet.component'
+      );
+      this.vcr.createComponent(LazySvgSpriteSheetComponent);
+    });
+  }
 }
