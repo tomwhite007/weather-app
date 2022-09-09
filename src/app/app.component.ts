ce16e2d3a0b13fdf766eabae3e117e1d06ea0af7
Tom White <tomwhite007@gmail.com>
2022-09-09 08:56:12 +0100

    refactor: make spritesheet more lazy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit, ViewContainerRef } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnInit,
+  ViewContainerRef,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements AfterViewInit {
   title = 'Weather App';
 
   constructor(private vcr: ViewContainerRef) {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     setTimeout(async () => {
       const { LazySvgSpriteSheetComponent } = await import(
         './core/lazy-svg-sprite-sheet/lazy-svg-sprite-sheet.component'
