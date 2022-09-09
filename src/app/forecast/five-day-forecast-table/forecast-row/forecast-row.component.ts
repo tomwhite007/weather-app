e725737e036274513e695f28c8ab0956fd562809
Tom White <tomwhite007@gmail.com>
2022-09-09 21:07:28 +0100

    feat: dynamic table

--- src/app/forecast/five-day-forecast-table/forecast-row/forecast-row.component.ts
+++ src/app/forecast/five-day-forecast-table/forecast-row/forecast-row.component.ts
@@ -1,9 +1,4 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  Input,
-  OnInit,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { ForecastTableRow } from '../../+state/forecast.models';
 
 @Component({
