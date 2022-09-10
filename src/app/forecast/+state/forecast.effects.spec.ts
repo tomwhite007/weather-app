0075fb9a6a4501b982b11cf5ac65848489378023
Tom White <tomwhite007@gmail.com>
2022-09-10 22:24:06 +0100

    test: effect happy and sad path

---
+++ src/app/forecast/+state/forecast.effects.spec.ts
@@ -0,0 +1,67 @@
+import { fakeAsync, flush, TestBed } from '@angular/core/testing';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { Action } from '@ngrx/store';
+import { provideMockStore } from '@ngrx/store/testing';
+import { hot } from 'jasmine-marbles';
+import { Observable, of, throwError } from 'rxjs';
+
+import * as ForecastActions from './forecast.actions';
+import { ForecastEffects } from './forecast.effects';
+import { mockAdaptedForecastTableDef } from './services/mocks/mock-adapted-forecast-table-def';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { mockForecastApiResult } from './services/mocks/mock-forecast-api-result';
+import { HttpClient } from '@angular/common/http';
+
+describe('ForecastEffects', () => {
+  let actions: Observable<Action>;
+  let effects: ForecastEffects;
+  let mockHttpClient = jasmine.createSpyObj('http', ['get']);
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        ForecastEffects,
+        provideMockActions(() => actions),
+        provideMockStore(),
+        { provide: HttpClient, useValue: mockHttpClient },
+      ],
+    });
+
+    effects = TestBed.inject(ForecastEffects);
+  });
+
+  describe('loadForecast$', () => {
+    it('should load Forecast and convert to view model', () => {
+      mockHttpClient.get.and.returnValue(of(mockForecastApiResult));
+      actions = hot('-a-|', {
+        a: ForecastActions.loadForecast({ city: 'London' }),
+      });
+
+      const expected = hot('-a-|', {
+        a: ForecastActions.loadForecastSuccess({
+          forecast: mockAdaptedForecastTableDef,
+        }),
+      });
+
+      expect(effects.loadForecast$).toBeObservable(expected);
+    });
+
+    it('should attempt to load Forecast and then log error', () => {
+      mockHttpClient.get.and.returnValue(throwError(() => new Error('test')));
+      actions = hot('-a-|', {
+        a: ForecastActions.loadForecast({ city: 'London' }),
+      });
+
+      const expected = hot('-(a|)', {
+        a: ForecastActions.loadForecastFailure({
+          error: 'test',
+        }),
+      });
+
+      expect(effects.loadForecast$).toBeObservable(expected);
+    });
+  });
+});
