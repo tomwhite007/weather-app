a2f9e3b2e06c8afd1f43de8937ee6db31835a727
Tom White <tomwhite007@gmail.com>
2022-09-10 11:25:01 +0100

    feat: new model adapter v1

--- src/app/forecast/+state/services/forecast-adapter.service.ts
+++ src/app/forecast/+state/services/forecast-adapter.service.ts
@@ -2,6 +2,17 @@ import { Injectable } from '@angular/core';
 import { ForecastElement } from '../forecast.models';
 import { FiveDayForecastApiResult } from './models/five-day-forecast-api-result';
 
+interface ResponsiveTitles {
+  nameDesktop: string;
+  nameMobile: string;
+}
+
+interface RowTitleLookup {
+  temperature: ResponsiveTitles;
+  windspeed: ResponsiveTitles;
+  weatherDescription: ResponsiveTitles;
+}
+
 @Injectable({
   providedIn: 'any',
 })
@@ -15,6 +26,12 @@ export class ForecastAdapterService {
     'Friday',
     'Saturday',
   ];
+
+  readonly rowTitleLookup = {
+    temperature: { nameDesktop: 'Temperature (°C)', nameMobile: '°C' },
+    windspeed: { nameDesktop: 'Windspeed (mph)', nameMobile: 'Wind mph' },
+    weatherDescription: { nameDesktop: 'Weather', nameMobile: '' },
+  };
 
   adaptWeatherApiToForecastState(
     apiResult: FiveDayForecastApiResult
@@ -35,4 +52,71 @@ export class ForecastAdapterService {
         }))
     );
   }
+
+  adaptForecastArrayToCrosstabModel(forecast: ForecastElement[]) {
+    return {
+      dayColumns: [...forecast.map((row) => row.id)],
+      headingRow: convertColumn(forecast, 'day'),
+      weatherInfoRows: this.weatherInfoRows(forecast),
+
+      // [
+      //   {
+      //     type: 'text',
+      //     nameDesktop: 'Temperature (°C)',
+      //     nameMobile: '°C',
+      //     ...convertColumn(forecast, 'temperature'),
+      //   },
+      //   {
+      //     type: 'text',
+      //     nameDesktop: 'Windspeed (mph)',
+      //     nameMobile: 'Wind mph',
+      //     ...convertColumn(forecast, 'windspeed'),
+      //   },
+      //   {
+      //     type: 'text',
+      //     nameDesktop: 'Weather',
+      //     ...convertColumn(forecast, 'weatherDescription'),
+      //   },
+      // ],
+      iconRow: {},
+      // {
+      //   type: 'icon',
+      //   ...Object.assign(
+      //     {},
+      //     ...forecast.map((row) => ({
+      //       [row.id]: iconUrl.replace('{icon}', row.weatherIcon),
+      //     }))
+      //   ),
+      // },
+      // ] as ForecastTableRow[],
+      // message: state.loading
+      //   ? 'Loading'
+      //   : state.loaded
+      //   ? null
+      //   : state.error
+      //   ? 'Oops. There was an error getting the forecast.'
+      //   : 'Select a City above to see a five day forecast',
+    };
+  }
+
+  private weatherInfoRows(forecast: ForecastElement[]) {
+    const weatherInfoRows = [];
+    let key: keyof RowTitleLookup;
+    for (key in this.rowTitleLookup) {
+      if (Object.prototype.hasOwnProperty.call(this.rowTitleLookup, key)) {
+        weatherInfoRows.push({
+          ...this.rowTitleLookup[key],
+          ...convertColumn(forecast, key),
+        });
+      }
+    }
+    return weatherInfoRows;
+  }
+}
+
+function convertColumn(
+  rows: ForecastElement[],
+  key: keyof ForecastElement
+): { [key: string]: string } {
+  return Object.assign({}, ...rows.map((row) => ({ [row.id]: row[key] })));
 }
